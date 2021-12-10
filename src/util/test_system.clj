f7d30f0d8763f11bf8f050c2ae8a2511386b8929
Arthur <arthurdsb_@hotmail.com>
2021-12-10 12:21:39 -0300

    chore(tests): add server integration test system

---
+++ src/util/test_system.clj
@@ -0,0 +1,37 @@
+(ns util.test-system
+  (:require [integrant.repl :as ig-repl]
+            [short.server]
+            [integrant.core :as ig]
+            [taoensso.timbre :as timbre]))
+
+(def environment-vars
+  {:port 4000
+   :database_host ""
+   :database_port ""
+   :database_user ""
+   :database_backend "mem"
+   :database_id "xis"
+   :database_password ""
+   :database_name ""})
+
+(def config-map
+  {:server/jetty {:handler (ig/ref :short/app)
+                  :port (:port environment-vars)}
+   :short/app {:database (ig/ref :db/postgres)}
+   :db/postgres {:host (:database_host environment-vars)
+                 :port (:database_port environment-vars)
+                 :user (:database_user environment-vars)
+                 :backend (:database_backend environment-vars)
+                 :id (:database_id environment-vars)
+                 :password (:database_password environment-vars)
+                 :dbname (:database_name environment-vars)}})
+
+(ig-repl/set-prep!
+ (fn [] config-map))
+
+(def reset-all ig-repl/reset-all)
+
+(defn start-fixture [config]
+  (timbre/set-level! :error)
+  (reset-all)
+  config)
--- test/test_helpers.clj
+++ test/test_helpers.clj
@@ -1,6 +1,9 @@
 (ns test-helpers
   (:require [short.database :refer [schema]]
-            [datahike.api :as d]))
+            [datahike.api :as d]
+            [integrant.repl.state :as state]
+            [muuntaja.core :as m]
+            [ring.mock.request :as mock]))
 
 (defn create-in-memory-db [id]
   (let [config {:store {:backend :mem
@@ -9,3 +12,17 @@
         connection (d/connect config)]
     (d/transact connection schema)
     connection))
+
+(defn endpoint-test
+  ([method uri]
+   (endpoint-test method uri {}))
+  ([method uri opts]
+   (let [app (-> state/system :short/app)
+         request
+         (app (-> (mock/request method uri)
+                  (cond-> (:auth opts) (mock/header :Authorization (str "Token " (:token (:auth opts))))
+                          (:body opts) (mock/json-body (:body opts)))))]
+     (update request :body (partial m/decode "application/json")))))
+
+(defn database-atom []
+  (-> state/system :db/postgres))
